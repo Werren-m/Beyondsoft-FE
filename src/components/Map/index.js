eea29d7e4431510f95aa283be145d95a6838a113
Werren <w_erren@hotmail.com>
2023-01-24 02:19:58 +0700

    Add map component

---
+++ src/components/Map/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Marker,
+  ZoomableGroup
+} from "react-simple-maps";
+
+import './styles.css'
+
+import geoUrl from "./geo.json"
+
+const MapChart = (props) => {
+
+  const { latitude, longitude } = props.address
+
+  return (
+    <ComposableMap style={props.style}>
+      <ZoomableGroup
+          zoom={4}
+          center={[latitude, longitude]}
+      >
+        <Geographies geography={geoUrl}>
+          {({ geographies }) =>
+            geographies.map((geo) => (
+              <Geography key={geo.rsmKey} geography={geo} />
+            ))
+          }
+        </Geographies>
+        <Marker coordinates={[latitude, longitude]}>
+          <circle r={8} fill="#F53" />
+        </Marker>
+      </ZoomableGroup>
+    </ComposableMap>
+  );
+};
+
+export default MapChart
+
