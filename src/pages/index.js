b0ae355bbb323b635949d4c4aacb4907a1f5d7dc
Werren <w_erren@hotmail.com>
2023-01-29 03:26:04 +0700

    Add pages main component

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import 'react-datetime/css/react-datetime.css'
+import 'react-leaflet-markercluster/dist/styles.min.css'
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import React from 'react'
+
+import Mappage from './MapPage'
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/map" element={<Mappage />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
