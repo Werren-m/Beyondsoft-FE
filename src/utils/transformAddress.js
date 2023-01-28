b2e4a20a2313170f4567f677277233c27abaf3eb
Werren <w_erren@hotmail.com>
2023-01-29 03:25:39 +0700

    Modify transformer to fit antd autocomplete

--- src/utils/transformAddress.js
+++ src/utils/transformAddress.js
@@ -2,7 +2,7 @@ export const transformAddress = (data) => {
   return data.map((addressData) => {
     return {
       ...addressData,
-      label: addressData.address,
+      value: addressData.address,
     }
   })
 }
